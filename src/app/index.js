aac551e64c1098d6d9282c053b5ca7cf44c0a20b
Alexey-petr0v <Alexey-petr0v@yandex.ru>
2020-07-22 22:52:48 +0700

    Донастроен репозиторий, подключена и протестирована графическая библиотека chartist

---
+++ src/app/index.js
@@ -0,0 +1,6 @@
+
+import '../bower_components/chartist/dist/chartist.min.css'
+import '../bower_components/chartist/dist/scss/chartist.scss'
+import '../bower_components/chartist/dist/scss/settings/_chartist-settings.scss'
+import './algorithm.js'
+import '../style/style.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: __dirname + "/src/app/index.js",
   output: {
@@ -7,10 +9,35 @@ module.exports = {
     publicPath: '/'
   },
   module: {
-      rules: [ 
-      ]
-  },
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          },
+        ]
+      }
+  ]
+ },
   plugins: [
+    new MiniCssExtractPlugin({
+        filename: '[name].css',
+    }),
       new HtmlWebpackPlugin({
           template: __dirname + "/src/public/index.html",
           inject: 'body'
